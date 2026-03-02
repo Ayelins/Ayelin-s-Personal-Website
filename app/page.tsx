4a443618388465a1ff4d71ea7f31da248db94987
Ayelins <avell5@uic.edu>
2026-03-02 12:59:01 -0600

    Add home page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,24 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-4">
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-      <Link href="/projects">Projects</Link>
+    <div>
+      <div className="flex flex-row gap-4">
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/projects">Projects</Link>
+      </div>
+
+      <main>
+        <h1 className="text-5xl font-bold">Ayelin Vellikara</h1>
+        <p>Welcome to my personal website</p>
+        <Image
+          src="/profile.jpg"   // file must be in /public
+          alt="Profile"
+          width={220}
+          height={220}
+          className="mt-10"
+        />
+      </main>
     </div>
   );
 }
